41549ae445f76ae1dc95adee0162bbf29c438a64
ICHA-PC\ICHA <riska150600394@gmail.com>
2016-12-30 14:10:41 +0700

    Bikin Form Login

---
+++ dtb_perusahaan_sawit/dtb_perusahaan_sawit/Login.vb
@@ -0,0 +1,51 @@
+﻿Public Class Login
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" And TextBox2.Text = "" Then
+            MsgBox("User Name & Password Harus Di Isi", MsgBoxStyle.OkOnly, "Perhatian")
+        Else
+            If TextBox1.Text = "" Then
+                MsgBox("User Name Belum Di Isi", MsgBoxStyle.OkOnly, "Perhatian")
+            Else
+                If TextBox2.Text = "" Then
+                    MsgBox("Password Belum Di Isi", MsgBoxStyle.OkOnly, "Perhatian")
+                Else
+                    If TextBox1.Text = "gengs" And TextBox2.Text = "gengs123" Then
+                        MsgBox("Welcome", MsgBoxStyle.OkOnly, "Sukses")
+                        Beranda.Show()
+                        Call kosong()
+                        Me.Hide()
+                    Else
+                        MsgBox("User Name / Password yang Anda Masukkan Salah", MsgBoxStyle.OkOnly, "Perhatian")
+                        Call kosong()
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Call kosong()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+    Private Sub Login_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call kosong()
+    End Sub
+    Sub kosong()
+        TextBox1.text = ""
+        TextBox2.Text = ""
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
--- dtb_perusahaan_sawit/dtb_perusahaan_sawit/My Project/Application.Designer.vb
+++ dtb_perusahaan_sawit/dtb_perusahaan_sawit/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.dtb_perusahaan_sawit.keuangan
+            Me.MainForm = Global.dtb_perusahaan_sawit.Login
         End Sub
     End Class
 End Namespace
